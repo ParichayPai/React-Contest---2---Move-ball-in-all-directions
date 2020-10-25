9f25a60f9cf3778e85b62f1164a7013696ea8a1a
Parichay Pai <39957257+ParichayPai@users.noreply.github.com>
2020-10-25 19:30:11 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,60 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  React.useEffect(
+    (document.onkeydown = (event) => {
+      event = event || window.event;
+
+      if (event.keyCode === 40) {
+        setY(y + 5);
+        setBallPosition({
+          left: ballPosition.left,
+          top: y + "px"
+        });
+      } else if (event.keyCode === 38) {
+        setY(y - 5);
+        setBallPosition({
+          left: ballPosition.left,
+          top: y + "px"
+        });
+      } else if (event.keyCode === 37) {
+        setX(x - 5);
+        setBallPosition({
+          left: x + "px",
+          top: ballPosition.top
+        });
+      } else if (event.keyCode === 39) {
+        setX(x + 5);
+        setBallPosition({
+          left: x + "px",
+          top: ballPosition.top
+        });
+      }
+    })
+  );
+  const reset = () => {
+    setX(0);
+    setY(0);
+    setBallPosition({ left: "0px", top: "0px" });
+  };
+  const start = () => {
+    setRenderBall(true);
+  };
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button onClick={start} className="start">
+          Start
+        </button>
+      );
+  };
 
   return (
     <div className="playground">
